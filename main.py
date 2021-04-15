caa218d3e59daca17b98b62b8665296f0c23ceab
karolpociask <karolpociask@outlook.com>
2021-04-15 13:53:09 +0200

    v0.1 - funkcja generujaca tablice

--- main.py
+++ main.py
@@ -7,15 +7,14 @@ def genTab(x_0,x_p, n):
     pom = (x_p - x_0) / (n - 1)
     macierz = np.array([x_0])
 
-    for i in range(1, n, 1):
+    for x in range(1, n, 1):
         macierz = np.block([
-            [macierz,
-            i * pom],
+            [macierz, x * pom],
         ])
-    return i, macierz
+    return x, macierz
 
 #print(genTab(0,1,4))
 
 
-lp, tablica = genTab(0, 4, 10)
+lp, tablica = genTab(0, 4, 100)
 print (lp, tablica)
